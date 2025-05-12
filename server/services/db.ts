346f3ba8db41c68e8b2f688b95c8c4f68c813696
Lippi99 <lipesalomao99@gmail.com>
2025-05-12 14:23:13 -0300

    feat: added ssl

--- server/services/db.ts
+++ server/services/db.ts
@@ -7,4 +7,5 @@ export const pool = new Pool({
   user: process.env.DB_USER as string,
   password: process.env.DB_PASSWORD as string,
   database: process.env.DB_NAME as string,
+  ssl: { rejectUnauthorized: false },
 });
